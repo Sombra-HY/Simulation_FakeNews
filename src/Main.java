98f563715553440a376bd9f1cf9c9a517d6aab16
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-04-29 16:12:04 -0300

    att

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-
+import Pessoas.*;
+import IAs.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa a = new Pessoa();
+        Pessoa b = new Pessoa();
+        Pessoa c = new Pessoa();
+        Pessoa d = new Pessoa();
+
     }
 }
