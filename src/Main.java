d561d1b940b8b13180d41f41a04f7b56670a3637
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-04-30 13:23:21 -0300

    Draw map

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 import Pessoas.*;
 import IAs.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Pessoa a = new Pessoa();
-        Pessoa b = new Pessoa();
-        Pessoa c = new Pessoa();
-        Pessoa d = new Pessoa();
+
+        Mundo teste = new Mundo();
+        Date tempo_inicio = new Date();
+
+        while (true){
+            Date current_time = new Date();
+            /**
+             * a cada interação atualiza-se o tempo
+             */
+            System.out.println("=======================================");
+            System.out.println("Tempo de Simulação: "
+                    + (current_time.getTime()
+                    - tempo_inicio.getTime()) / 1000);
+            System.out.println("=======================================");
+            teste.desenhaMundo();
+
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){
+                e.getMessage();
+            }
+        }
 
     }
 }
--- src/Mundo.java
+++ src/Mundo.java
@@ -1,4 +1,55 @@
 public class Mundo {
     public int[][] map = new int[30][60];
 
+    public Mundo(){
+        refazMudo();
+    }
+
+    void atualizaMundo(){
+
+    }
+    void desenhaMundo(){
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[i].length; j++){
+                switch(map[i][j]){
+                    case 0:
+                        System.out.print("\033[47m \033[0m");
+                        break;
+                    case 1:
+                        System.out.print("\033[47m \033[0m");
+                        break;
+                    case 2:
+                        System.out.print("\033[45m \033[0m");
+                        break;
+                    case 3:
+                        System.out.print("\033[44m \033[0m");
+                        break;
+                    case 4:
+                        System.out.print("\033[41m \033[0m");
+                        break;
+                    case 5:
+                        System.out.print("\033[43m \033[0m");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    void refazMudo(){
+        for(int eixoX = 0; eixoX < map.length; eixoX++ )
+            for (int eixoY = 0; eixoY < map[0].length; eixoY++)
+                map[eixoX][eixoY] = 0;
+    }
+
+
+
+
+
+
 }
